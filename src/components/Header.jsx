fc29ae9d05783ba150cc01b995d77d9dd9088a7b
samirjabib <samirdev9669@gmail.com>
2022-12-22 15:58:27 -0500

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -64,7 +64,7 @@ export const Header = ({auth}) => {
                         )
                     })}
                     <div className="flex flex-col items-center justify-center content-end text-center  relative top-[30%] gap-2 w-full">
-                        <p className="uppercase font-serif text-xs mb-4">¡¡No te pierdas de nuestras promociones!!</p>
+                        <p className="uppercase font-serif text-lg mb-4 font-semibold">¡¡No te pierdas de nuestras promociones!!</p>
                         <input placeholder="E-MAIL" className=" p-2 border-b border-black w-full text-sm"/>
                         <button className="mt-4 border-2 border-black w-full bg-bg_dark_primary text-bg_light_primary font-serif font-bold text-lg p-[.2em]">Suscribete</button>
                     </div>
