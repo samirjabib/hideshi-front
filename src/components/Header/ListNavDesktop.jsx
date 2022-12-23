d87908375df4ff2953cd4ab8d1ce7148c2636c88
samirjabib <samirdev9669@gmail.com>
2022-12-23 16:02:31 -0500

    add product details dynamic

--- src/components/Header/ListNavDesktop.jsx
+++ src/components/Header/ListNavDesktop.jsx
@@ -4,7 +4,7 @@ export const ListNavDesktop = ({navLinks, setOpen}) => {
 
 
     return(
-        <ul className='hidden md:flex gap-8'>
+        <ul className='hidden md:flex gap-8  mr-36'>
             <NavLinks navLinks={navLinks} desktop={true} setOpen ={ setOpen }/>
         </ul>
     )
--- src/features/shop/components/ProductCard.jsx
+++ src/features/shop/components/ProductCard.jsx
@@ -1,16 +1,19 @@
+import { useNavigate } from "react-router-dom";
 
 export const ProductCard = ({product}) => {
 
     const { id, name, img, price } = product
+    console.log(id)
 
-  
+    const navigate = useNavigate();
     
     return (
         <div className=' cursor-pointer transition-transform duration-200 hover:scale-105'>
             <img 
                 src={img}
                 alt="image"
-                className=" w-full h-[80%] flex items-center justify-center object-contain "
+                className=" w-full h-[80%] flex items-center justify-center object-contain"
+                onClick={ () => navigate(`/shop/${id}`)}
             />
             <div className=" w-full flex-col text-center flex justify-center items-center uppercase h-[20%] ">
                 <h2 className="font-bold text-sm transition-all hover:border-b w-40 ">{name}</h2>
