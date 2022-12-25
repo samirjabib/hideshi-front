e671b9d3fee8a7fe862889ff0452685e19124ecd
samirjabib <samirdev9669@gmail.com>
2022-12-25 15:34:37 -0500

    fx styles sidebar

--- src/components/SideBarBag.jsx
+++ src/components/SideBarBag.jsx
@@ -7,7 +7,7 @@ export const SideBarBag = ( {onHandleBag, openBag}) => {
         <div className={`fixed left-0 top-0 w-11/12  max-w-[18rem] h-screen overflow-scroll bg-bg_light_primary shadow-2xl  z-[999] 
             transition-all duration-300 flex-col flex p-4
         ${openBag ? 'left-0': 'left-[-100%]'} `}>
-            <div className="w-[15.7rem] flex fixed flex-row justify-between mb-2 bg-bg_light_primary top-0 h-12 py-4 z-[999]">
+            <div className="w-[15.7rem] flex fixed flex-row justify-between mb-2 bg-bg_light_primary top-0 h-12 py-4 z-[999] mt-4">
                 <h6 className=" text-sm font-semibold flex flex-row self-end">SHOPPING BAG</h6>
                 <AiOutlineClose 
                         className={`self-end font-bold `} 
@@ -15,7 +15,7 @@ export const SideBarBag = ( {onHandleBag, openBag}) => {
                         size={22}
                 />
             </div>
-            <span className="border bg-text_gray_for_span mt-10 w-full"></span>
+            <span className="border bg-text_gray_for_span mt-14 w-full "></span>
             <CartShop/>
             <span className="border bg-text_gray_for_span mt-10 w-full"></span>
 
