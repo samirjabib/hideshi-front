acc77ffa5e1eb0d811173080863409fb567947c3
samirjabib <samirdev9669@gmail.com>
2022-12-25 15:17:58 -0500

    add cart to sidebnar shop

--- src/components/CartShop/Card.jsx
+++ src/components/CartShop/Card.jsx
@@ -1,22 +1,28 @@
 export const Card = ( { product }) => {
     console.log(product)
-    const { id, name, img, price, quantity = '', category='coleccion'} = product
+    const { id, name, img, price, quantity = 4 , category='coleccion', } = product
+
+    const priceNumber = parseFloat(price)
+
+    const total = quantity * priceNumber;
 
     return(
-        <div className="gap-2 flex flex-row  p-2">
-            <img src={img} alt={name} className='w-[35%] object-contain'/>
-            <div>
+        <div className="gap-2 flex flex-row p-2 mt-8">
+            <img src={img} alt={name} className='w-[35%] object-contain shadow'/>
+            <div className="w-[75] flex flex-col">
                 <p>{name}</p>
                 <span>{category}</span>
                 <div>
-                    <div>
-                        <button>--</button>
-                        <span>total</span>
-                        <button>+</button>
+                    <div className="flex flex-row justify-between">
+                        <div>
+                            <button>--</button>
+                            <span>{quantity}</span>
+                            <button>+</button>
+                        </div>
+
+                        <div className="text-sm text-text_gray_for_span">COP <span className="text-black">{total}K</span> </div>
                     </div>
-                    <span>{price}</span>
                 </div>
-                <span>{quantity}</span>
             </div>
  
         </div>
