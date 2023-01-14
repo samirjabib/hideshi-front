224998ac5fb69e3ba2f2a796f6c19c0fa1bdc128
samirjabib <samirdev9669@gmail.com>
2023-01-14 13:28:33 -0500

    add

--- src/features/dashboard/component/FormCrud.jsx
+++ src/features/dashboard/component/FormCrud.jsx
@@ -1,5 +1,6 @@
 import { useForm } from "../../../hooks"
 import { InputCrud } from "./InputCrud"
+import { DropDown } from "./DropDown"
 
 
 const crudFormFields = {
@@ -45,11 +46,11 @@ export const FormCrud = () => {
                 className="grid grid-cols-1 md:grid-cols-4 mb-6 md:mb-8 w-full"
             >    
                 <label 
-                    className="block mb-2 text-base text-bg_dark_primary">Details</label>
+                    className="block mb-2 text-sm text-bg_dark_primary">Details</label>
                 <textarea 
                     rows="4" 
-                    className="block p-2.5 w-full text-sm col-span-3 text-gray-900 rounded-lg bg-[#f4f5f7]  outline-none" 
-                    placeholder="Write details  here...">
+                    className="block p-2.5 w-full text-sm col-span-3 text-gray-900 rounded-lg bg-[#f4f5f7] outline-none" 
+                    placeholder="Write details here...">
                 </textarea>
             </div>
         </form>
--- src/features/dashboard/component/InputCrud.jsx
+++ src/features/dashboard/component/InputCrud.jsx
@@ -1,7 +1,7 @@
 export const InputCrud = ({placeholder, name, label}) => {
     return(
         <div className="grid grid-cols-1 md:grid-cols-4 mb-6 md:mb-8">
-            <label className="col-span-1">Product Title</label>
+            <label className="col-span-1 text-sm">Product Title</label>
             <input 
                 className="bg-[#f4f5f7] rounded px-2 py-3 w-full outline-none col-span-3"  
             />
