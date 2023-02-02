9d3f7361ac5825737f8732c59f2e9158b26a53d7
samirjabib <samirdev9669@gmail.com>
2023-02-01 21:51:04 -0500

    add fix

--- src/features/dashboard/component/UploadPicture.jsx
+++ src/features/dashboard/component/UploadPicture.jsx
@@ -15,8 +15,9 @@ export const UploadPicture = ({ onFileInputChange, filesImg }) => {
 
     return(
         <>
-            <div className="grid grid-cols-1 md:grid-cols-4 p-6 grid-rows-4 ">
-                <p className=" col-span-4 md:col-span-1 relative top-5 md:top-0  text-base ">Product Image</p>
+            <div className="grid grid-cols-1 md:grid-cols-4 p-6  ">
+                <p className=" col-span-4 md:col-span-1  md:top-0  text-base ">Product Image</p>
+                <div className="w-full border-2  col-span-3">
                 <input 
                     type="file"
                     ref={fileInputRef}
@@ -24,41 +25,36 @@ export const UploadPicture = ({ onFileInputChange, filesImg }) => {
                     className="hidden"
                     multiple
                 />
-                    <div 
-                        className="border-2 border-dashed md:col-span-3   mx-auto row-span-4 items-center justify-center grid p-8 gap-y-2 cursor-pointer "
-                        onClick={ () => fileInputRef.current.click()}
-                    >
-                        <AiOutlineCloudUpload 
-                            size={32}
-                            className='w-full'
-                        />
-                        <p className="w-full text-center ">Drag your image here</p>
-                        <span className="text-text_gray_for_span text-[.7rem] text-center w-full">
-                            (only *.jpeg and *.png images will be accepted)
-                        </span>
-                    
-                    </div>
-                    
-                        
-                    <div className="col-start-2 col-end-5  mt-4 text-center ">
-                         <h4 className={`${!!filesImg ? '' : 'hidden'} text-sm`}>Img Uploading</h4>
-                        {
-                            !!filesImg && filesImg.map( (file, index) => {
-
-                                console.log(file)
-
-                                return(
-                                    <p 
-                                    key={index}
-                                    className='text-xs text-gray'
-                                    >
-                                        {file.name}
-                                    </p>
-                                )
-                            })
-                        }
-                        
-                    </div> 
+                <div 
+                    className="border-2 border-dashed md:col-span-3 mx-auto grid-span-4 items-center justify-center grid p-8 gap-y-2 cursor-pointer "
+                    onClick={ () => fileInputRef.current.click()}
+                >
+                    <AiOutlineCloudUpload 
+                        size={32}
+                        className='w-full'
+                    />
+                    <p className="w-full text-center ">Drag your image here</p>
+                    <span className="text-text_gray_for_span text-[.7rem] text-center w-full">
+                        (only *.jpeg and *.png images will be accepted)
+                    </span>
+                </div>
+                <div className="col-start-2 col-end-5  mt-4 text-center  border-2 border-black">
+                        <h4 className={`${!!filesImg ? '' : 'hidden'} text-sm`}>Img Uploading</h4>
+                    {
+                        !!filesImg && filesImg.map( (file, index) => {
+                            return(
+                                <p 
+                                key={index}
+                                className='text-xs text-gray-600 border-2 border-black'
+                                >
+                                    {file.name}
+                                </p>
+                            )
+                        })
+                    }
+                </div> 
+                </div>
+      
             </div>
 
    
